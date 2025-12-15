5b45244b02072f17c09d423906d5af54f055edeb
savelijfeklistov15-creator <savelijfeklistov15@gmail.com>
2025-12-15 22:19:34 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
-let currentProduct = null;
+unction show(id) {
+  document.querySelectorAll('.screen').forEach(el => {
+    el.style.display = 'none';
+  });
 
-function show(id) {
-  document.getElementById("shop").style.display = "none";
-  document.getElementById("cart").style.display = "none";
-  document.getElementById("profile").style.display = "none";
-  document.getElementById("product").style.display = "none";
-
-  document.getElementById(id).style.display = "block";
+  document.getElementById(id).style.display = 'block';
 }
 
-// открыть товар
-function openProduct(title, price) {
-  currentProduct = { title, price };
-  document.getElementById("productTitle").innerText = title;
-  document.getElementById("productPrice").innerText = price + " ₽";
-  show("product");
+function openProduct() {
+  show('product');
 }
 
-// старт
-show("shop");
+// ГАРАНТИЯ СТАРТА
+document.addEventListener('DOMContentLoaded', () => {
+  show('shop');
+});
